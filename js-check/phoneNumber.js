2e48d8f991f606f25bd6100f37d48cd5682e8ccc
Rhuan Santos <rhuansantosdev@gmail.com>
2018-01-18 20:23:30 -0500

    validating phone number

--- js-check/phoneNumber.js
+++ js-check/phoneNumber.js
@@ -8,7 +8,13 @@
 // remove dots as in "321.321.4321"
 
 const phoneNumber = (pNum) => {
-
+  const number = pNum.replace(/\D+/g, '');
+  const countryCode = number.charAt(0);
+  const reg = new RegExp(/^[2-9]\d{2}[2-9]\d{2}\d{4}$/).test(number);
+  if(number.length >= 11) {
+    return countryCode == 1 ? number.substr(1) : null;
+  }
+    return reg ? number : null;
 }
 
 module.exports = phoneNumber
