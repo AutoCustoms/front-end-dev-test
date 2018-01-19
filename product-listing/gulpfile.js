f778940d86ad0a3cbf4afec935e39bc9669b1a2c
Rhuan Santos <rhuansantosdev@gmail.com>
2018-01-19 13:43:19 -0500

    update gulp file and using stylus as pre-processor

--- product-listing/gulpfile.js
+++ product-listing/gulpfile.js
@@ -1,46 +1,29 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const stylus = require('gulp-stylus');
 const browserSync = require('browser-sync').create();
-const babel = require('babelify');
-const browserify = require('browserify')
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const minify = require('gulp-minifier');
 
-gulp.task('scss', () => {
-  return gulp.src('src/scss/main.scss')
-  .pipe(sass().on('error', sass.logError))
+gulp.task('pre-processor', () => {
+  return gulp.src('src/stylus/main.styl')
+  .pipe(stylus())
   .on('error', swallowError)
   .pipe(gulp.dest('src/css'))
   .pipe(browserSync.stream())
 
 });
 
-gulp.task('es6', () => {
-  browserify('src/js/main.js')
-    .transform('babelify', {
-      presets: ['es2015']
-    })
-    .bundle()
-    .on('error', swallowError)
-    .pipe(source('main.js'))
-    .pipe(buffer())
-    .pipe(gulp.dest('src/js/build'));
-});
-
 gulp.task('serve', () => {
     browserSync.init({
         server: "./src"
     });
-    gulp.watch("src/scss/**", ['scss']);
-    gulp.watch("src/js/**", ['es6']);
-    gulp.watch("src/js/build/**", ['es6']).on('change', browserSync.reload);
+    gulp.watch("src/stylus/**", ['pre-processor']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
-
 });
 
 gulp.task('build', function() {
-  return gulp.src(['src/**/*', '!src/js/core/', '!src/js/main.js']).pipe(minify({
+  return gulp.src(['src/**/*']).pipe(minify({
     minify: true,
     collapseWhitespace: true,
     conservativeCollapse: true,
@@ -58,6 +41,4 @@ function swallowError (error) {
   this.emit('end')
 }
 
-gulp.task('default', ['serve'], () => {
-
-});
+gulp.task('default', ['serve']);
